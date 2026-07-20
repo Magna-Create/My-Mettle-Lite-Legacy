ca562f0a46a07275359e8aec8be64355f04127c7
Magna-Create <53941598+Magna-Create@users.noreply.github.com>
2026-07-20 14:13:40 +0100

    Register native rest timer plugin

--- android/app/src/main/java/dev/kian/gymapp/MainActivity.java
+++ android/app/src/main/java/dev/kian/gymapp/MainActivity.java
@@ -1,5 +1,25 @@
 package dev.kian.gymapp;
 
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(RestTimerNotificationsPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        AppVisibility.setForeground(true);
+    }
+
+    @Override
+    protected void onPause() {
+        AppVisibility.setForeground(false);
+        super.onPause();
+    }
+}
