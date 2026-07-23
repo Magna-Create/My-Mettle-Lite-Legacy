66397f9f9e906f479a33d605ca736d8666f731d2
Magna-Create <53941598+Magna-Create@users.noreply.github.com>
2026-07-23 23:18:34 +0100

    Remove retired Gemma E4B files on upgrade

--- android/app/src/main/java/dev/kian/gymapp/MainActivity.java
+++ android/app/src/main/java/dev/kian/gymapp/MainActivity.java
@@ -4,9 +4,14 @@ import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 
+import java.io.File;
+
 public class MainActivity extends BridgeActivity {
+    private static final String RETIRED_E4B_FILE = "gemma-4-E4B-it.litertlm";
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        removeRetiredGemmaE4B();
         registerPlugin(RestTimerNotificationsPlugin.class);
         registerPlugin(MaisDeviceStatePlugin.class);
         registerPlugin(MaisModelRuntimePlugin.class);
@@ -15,6 +20,17 @@ public class MainActivity extends BridgeActivity {
         registerPlugin(MaisLiteRtRuntimePlugin.class);
         registerPlugin(MaisEmbeddingRuntimePlugin.class);
         super.onCreate(savedInstanceState);
+    }
+
+    private void removeRetiredGemmaE4B() {
+        File modelDirectory = new File(getFilesDir(), "mais-models");
+        deleteIfPresent(new File(modelDirectory, RETIRED_E4B_FILE));
+        deleteIfPresent(new File(modelDirectory, RETIRED_E4B_FILE + ".part"));
+        deleteIfPresent(new File(modelDirectory, RETIRED_E4B_FILE + ".verified.json"));
+    }
+
+    private void deleteIfPresent(File file) {
+        if (file.exists() && !file.delete()) file.deleteOnExit();
     }
 
     @Override
